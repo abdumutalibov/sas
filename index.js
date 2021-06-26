34e00c6c49e8eaf7904bf2cb9dd7f17e462314de
Azimjon <dryden1309@gmail.com>
2021-06-26 23:53:46 +0500

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+//1 Write the following numbers in the programming language
+
+// 1. Quyidagi sonlarni dasturlash tilida yozing
+// a) 125   b) -345
+// c) 1,25 d) -0,2573
+// e) 2,7·10** f) 3,4·10-17
+console.log('dasasdasdasdasdsad')
